899194e1b9db7aca3fcd48a25a0501044630e1a2
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-05 12:11:57 +0100

    :sparkles: Get GitHub profile
    Log response and error objects to console to study structure

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,15 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+  .get("https://api.github.com/users/emkayDauda")
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
